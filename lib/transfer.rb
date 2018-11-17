bd7a9b4709f51dea882ea8dea290513b8d75a769
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-17 05:59:55 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,7 +10,7 @@ class Transfer
   end
 
   def valid?
-    @sender.valid? && @receiver.valid?
+    @sender.valid? && @receiver.valid? && @sender.balance - @amount > 0
   end
 
   def execute_transaction
